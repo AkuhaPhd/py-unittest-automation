2e75e6a516ddd0fbb1ccc4f677a269d48520af66
Akuhaa <akuha@islacare.co.uk>
2023-03-20 17:27:50 +0000

    tests

---
+++ test_main.py
@@ -0,0 +1,17 @@
+"""Test py fuction"""
+import unittest
+from main import sorted_squared_array
+
+
+
+class TestMLApi(unittest.TestCase):
+    def test_sorted_squared_array(self):
+        """
+        Testing end to end api feature detection module hit all relevant feature detection models and roi filter classes
+        """
+
+        self.assertEqual(sorted_squared_array([1, 2, 3, 5, 6, 8]), [1, 4, 9, 25, 36, 64])
+
+
+if __name__ == '__main__':
+    unittest.main()
