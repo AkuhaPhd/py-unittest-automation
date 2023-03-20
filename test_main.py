89737638dc3b90cc4019d98e91d35cf059c8f103
Akuhaa <akuha@islacare.co.uk>
2023-03-20 17:40:43 +0000

    pylint

--- test_main.py
+++ test_main.py
@@ -1,13 +1,17 @@
 """Test py fuction"""
 import unittest
+"""test module."""
 from main import sorted_squared_array
 
 
-
 class TestMLApi(unittest.TestCase):
+    """
+    test main py file.
+    """
     def test_sorted_squared_array(self):
         """
-        Testing end to end api feature detection module hit all relevant feature detection models and roi filter classes
+        Testing end to end api feature detection module hit all relevant feature detection
+        models and roi filter classes.
         """
 
         self.assertEqual(sorted_squared_array([1, 2, 3, 5, 6, 8]), [1, 4, 9, 25, 36, 64])
