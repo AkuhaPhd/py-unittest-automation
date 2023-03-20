14da38c7d71c421b5e7a468f32a1e92ee529b416
Akuhaa <akuha@islacare.co.uk>
2023-03-20 16:46:26 +0000

    pylint corrections

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-def sortedSquaredArray(array):
-	newarray = [x*x for x in array]
-	newarray.sort()
-	return newarray
+def sorted_squared_array(array):
+    """
+    function returns sorted squared array
+    """
+    newarray = [x*x for x in array]
+    newarray.sort()
+    return newarray
 
 if __name__ == "__main__":
-	arr = [1, 2, 3, 5, 6, 8]
-	out = sortedSquaredArray(arr)
-	print(arr)
+    """
+    test function
+    """
+    arr = [1, 2, 3, 5, 6, 8]
+    out = sorted_squared_array(arr)
+    print(arr)
